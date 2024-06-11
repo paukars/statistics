cf051bae16e39611b679416cdea73647801c6b0a
paukars <paulsoar19@gmail.com>
2024-06-10 23:39:53 -0600

    Added 2 mores views and their respectives templates to analize which distribution the histogram fits best

--- cursoDeVerano/histogramas/urls.py
+++ cursoDeVerano/histogramas/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from .views import histograma_de_malignos, histograma_de_benignos, histogramas_combinados
+from .views import (histograma_de_benignos, histograma_de_malignos,
+                    histogramas_combinados, malignos_curva, benignos_curva)
 
 urlpatterns = [
     path("histograma-maligno", histograma_de_malignos, name="histograma-maligno"),
     path("histograma-benigno", histograma_de_benignos, name="histograma-benigno"),
-    path("histograma-combinado", histogramas_combinados, name="histogramas_combinados")
+    path("histograma-combinado", histogramas_combinados, name="histogramas_combinados"),
+    path("malignos-curva", malignos_curva, name="malignos_curva"),
+    path("benignos-curva", benignos_curva, name="benignos_curva"),
 ]
--- cursoDeVerano/histogramas/views.py
+++ cursoDeVerano/histogramas/views.py
@@ -1,8 +1,12 @@
+import base64
 import io
-import numpy as np
+
 import matplotlib.pyplot as plt
+import numpy as np
+import seaborn as sns
 from django.http import HttpResponse
 from django.shortcuts import render
+from scipy import stats
 
 from .models import ReporteMedico
 
@@ -112,10 +116,143 @@ def histogramas_combinados(request):
 
     context = {
         "image_data": image_data,
-        "mean_perimeter_malignant" : mean_perimeter_malignant,
+        "mean_perimeter_malignant": mean_perimeter_malignant,
         "std_perimeter_malignant": std_perimeter_malignant,
         "mean_perimeter_benign": mean_perimeter_benign,
         "std_perimeter_benign": std_perimeter_benign,
     }
 
     return render(request, "histogramas_combinados.html", context)
+
+
+def malignos_curva(request):
+    records = ReporteMedico.objects.filter(Diagnosis="M")
+    perimeters = [record.Perimeter for record in records]
+
+    # Calculate mean and standard deviation for malignant diagnoses
+    mean_perimeter = np.mean(perimeters)
+    std_perimeter = np.std(perimeters)
+
+    # Fit different distributions and determine the best fit
+    distributions = [stats.expon, stats.lognorm, stats.chi2, stats.norm]
+    best_fit = None
+    best_ks_stat = np.inf
+
+    for distribution in distributions:
+        params = distribution.fit(perimeters)
+        ks_stat, p_value = stats.kstest(perimeters, distribution.cdf, args=params)
+        if ks_stat < best_ks_stat:
+            best_ks_stat = ks_stat
+            best_fit = distribution, params
+
+    # Create the histogram plot
+    plt.figure(figsize=(10, 9))
+    counts, bins, patches = plt.hist(
+        perimeters, bins=30, density=True, color="red", edgecolor="black"
+    )
+    plt.title("Histograma del perimetro para diagnosticos malignos")
+    plt.xlabel("Perimetro")
+    plt.ylabel("Frecuencia")
+    plt.grid(True, which="both", axis="x", color="gray", linestyle="-", linewidth=0.5)
+    plt.grid(True, which="both", axis="y", color="gray", linestyle="-", linewidth=0.5)
+    plt.xticks(bins, rotation="vertical")
+
+    # Save the histogram figure to a buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close()
+
+    buf.seek(0)
+    histogram_image_data = buf.getvalue()
+
+    # Q-Q plot for malignant perimeters
+    distribution, params = best_fit
+    plt.figure(figsize=(10, 9))
+    stats.probplot(perimeters, dist=distribution, sparams=params, plot=plt)
+    plt.title("Q-Q Plot para Diagnósticos malignos")
+    plt.xlabel(f"Cuantiles teoricos la Distribución {best_fit[0].name.capitalize()}")
+    plt.ylabel("Datos ordenados")
+    plt.grid(True, which="both", axis="x", color="gray", linestyle="-", linewidth=0.5)
+    plt.grid(True, which="both", axis="y", color="gray", linestyle="-", linewidth=0.5)
+    qq_buf = io.BytesIO()
+    plt.savefig(qq_buf, format="png")
+    plt.close()
+    qq_buf.seek(0)
+    qq_image_data = qq_buf.getvalue()
+
+    context = {
+        "histogram_image_data": histogram_image_data,
+        "qq_image_data": qq_image_data,
+        "mean_perimeter": mean_perimeter,
+        "std_perimeter": std_perimeter,
+        "best_fit_distribution": best_fit[0].name,
+    }
+
+    return render(request, "malignos_curva.html", context)
+
+
+def benignos_curva(request):
+    records = ReporteMedico.objects.filter(Diagnosis="B")
+
+    perimeters = [record.Perimeter for record in records]
+
+    # Calculate mean and standard deviation for benign diagnoses
+    mean_perimeter = np.mean(perimeters)
+    std_perimeter = np.std(perimeters)
+
+    # Fit different distributions and determine the best fit
+    distributions = [stats.norm, stats.expon, stats.lognorm, stats.chi2]
+    best_fit = None
+    best_ks_stat = np.inf
+
+    for distribution in distributions:
+        params = distribution.fit(perimeters)
+        ks_stat, p_value = stats.kstest(perimeters, distribution.cdf, args=params)
+        if ks_stat < best_ks_stat:
+            best_ks_stat = ks_stat
+            best_fit = distribution, params
+
+    # Create the histogram plot
+    plt.figure(figsize=(10, 9))
+    counts, bins, patches = plt.hist(
+        perimeters, bins=40, density=True, color="green", edgecolor="black"
+    )
+    plt.title("Histograma del perimetro para diagnosticos benignos")
+    plt.xlabel("Perimetro")
+    plt.ylabel("Frecuencia")
+    plt.grid(True, which="both", axis="x", color="gray", linestyle="-", linewidth=0.5)
+    plt.grid(True, which="both", axis="y", color="gray", linestyle="-", linewidth=0.5)
+    plt.xticks(bins, rotation="vertical")
+
+    # Save the histogram figure to a buffer
+    buf = io.BytesIO()
+    plt.savefig(buf, format="png")
+    plt.close()
+
+    buf.seek(0)
+    histogram_image_data = buf.getvalue()
+
+    # Q-Q plot for benign perimeters
+    distribution, params = best_fit
+    plt.figure(figsize=(10, 9))
+    stats.probplot(perimeters, dist=distribution, sparams=params, plot=plt)
+    plt.title("Q-Q Plot para Diagnósticos Benignos")
+    plt.xlabel(f"Cuantiles teoricos de la Distribución {best_fit[0].name.capitalize()}")
+    plt.ylabel("Datos ordenados")
+    plt.grid(True, which="both", axis="x", color="gray", linestyle="-", linewidth=0.5)
+    plt.grid(True, which="both", axis="y", color="gray", linestyle="-", linewidth=0.5)
+    qq_buf = io.BytesIO()
+    plt.savefig(qq_buf, format="png")
+    plt.close()
+    qq_buf.seek(0)
+    qq_image_data = qq_buf.getvalue()
+
+    context = {
+        "histogram_image_data": histogram_image_data,
+        "qq_image_data": qq_image_data,
+        "mean_perimeter": mean_perimeter,
+        "std_perimeter": std_perimeter,
+        "best_fit_distribution": best_fit[0].name,
+    }
+
+    return render(request, "benignos_curva.html", context)
